8326ec99627ee053e84348c2a57d11344e72ae78
Liron Newman <eesheesh@users.noreply.github.com>
2021-06-24 21:09:37 +0100

    Update build.bat to reflect new name

--- build.bat
+++ build.bat
@@ -23,13 +23,13 @@ pushd "%~dp0"
 rem Create a build directory, ignore errors.
 mkdir build 2>nul
 rem Find the version number by searching for the version set in the VBS file.
-for /f "tokens=4" %%a IN ('findstr /R /B /C:"Const Ver = \"[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*\"" gspstool.vbs') DO set ver=%%a
+for /f "tokens=4" %%a IN ('findstr /R /B /C:"Const Ver = \"[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*\"" PasswordSyncSupportTool.vbs') DO set ver=%%a
 rem Strip the double quotes (") from the version number.
 set ver=%ver:"=%
 echo Going to build PasswordSyncSupportTool version %ver%...
 
 rem Generate the build commandline.
-set buildcmd=..\..\tools\autoit\Aut2Exe\Aut2Exe.exe /in gspstool.au3 /icon pictographs-fire_extinguisher_inv.ico /comp 4 /pack /gui /execlevel requireadministrator /companyname Google /productname "Password Sync support tool" /fileversion %ver% /productversion %ver%
+set buildcmd=..\..\tools\autoit\Aut2Exe\Aut2Exe.exe /in PasswordSyncSupportTool.au3 /icon pictographs-fire_extinguisher_inv.ico /comp 4 /pack /gui /execlevel requireadministrator /companyname Google /productname "Password Sync support tool" /fileversion %ver% /productversion %ver%
 
 echo Build command base: %buildcmd%
 
